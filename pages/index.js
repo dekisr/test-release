260593c2f1b358f199286c4a89f4e4656ab25556
Hideki J <aledinai@gmail.com>
2022-08-18 16:14:03 -0300

    test

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,8 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
+        <h2>{process.env.NEXT_PUBLIC_VERCEL_URL}</h2>
+
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
